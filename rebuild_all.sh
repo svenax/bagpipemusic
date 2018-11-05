adcaab09caa61cebdc3ea0c2e9603929575b4917
Sven Axelsson <sven@axelsson.name>
2018-11-05 10:29:21 +0000

    Update rebuild_all.sh to use domusic

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -1,18 +1,19 @@
 # Rebuild all the tunes
 
-./makelily 0-0_marches/*.ly
-./makelily 2-4_marches/*.ly
-./makelily 3-4_marches/*.ly
-./makelily 4-4_marches/*.ly
-./makelily 6-8_marches/*.ly
-./makelily 9-8_marches/*.ly
-./makelily airs/*.ly
-./makelily hornpipes/*.ly
-./makelily jigs/*.ly
-./makelily others/*.ly
-./makelily piobaireachd/*.ly
-./makelily reels/*.ly
-./makelily scandinavian/*.ly
-./makelily sets/*.ly
-./makelily strathspeys/*.ly
-./makelily suites/*.ly
+domusic make 0-0_marches/*.ly
+domusic make 2-4_marches/*.ly
+domusic make 3-4_marches/*.ly
+domusic make 4-4_marches/*.ly
+domusic make 6-8_marches/*.ly
+domusic make 9-8_marches/*.ly
+domusic make airs/*.ly
+domusic make hornpipes/*.ly
+domusic make jigs/*.ly
+domusic make others/*.ly
+domusic make piobaireachd/*.ly
+domusic make reels/*.ly
+domusic make scandinavian/*.ly
+domusic make strathspeys/*.ly
+domusic make suites/*.ly
+domusic make \!collections/*.ly
+domusic make \!private/*.ly
